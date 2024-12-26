55bbd47a7cd757080a2b8fb6b3f91e33b92c0c2d
Prasad Nayak <prasahdk9@gmail.com>
2024-12-26 10:29:28 +0530

    s

--- src/main/kotlin/com/ms/config_server/ConfigServerApplication.kt
+++ src/main/kotlin/com/ms/config_server/ConfigServerApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class ConfigServerApplication
 
 fun main(args: Array<String>) {
